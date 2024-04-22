86c76ef73cae185727b6aa8e9e8bf60111b604be
GamingProVideos <68269384+GamingProVideos@users.noreply.github.com>
2024-04-22 19:40:40 +0100

    Checking Webhook

--- locales/en.lua
+++ locales/en.lua
@@ -1,5 +1,6 @@
 Locales['en'] = {
   ['Paperin'] = 'Press [~g~E~w~] to add Bank Paper to Stock',
   ['inkin'] = 'Press [~g~E~w~] to add Ink to Stock',
-  ['DiscordWebhookName'] = 'Counterfeit Cash Script'
+  ['DiscordWebhookName'] = 'Counterfeit Cash Script',
+  ['CutFakeCash'] = 'Cut Counterfeit Notes'
 }
