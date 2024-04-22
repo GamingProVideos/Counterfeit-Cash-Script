ac8dccf4164d7bf070f1520acff141273c82382a
GamingProVideos <68269384+GamingProVideos@users.noreply.github.com>
2024-04-22 19:43:34 +0100

    Test

--- locales/en.lua
+++ locales/en.lua
@@ -2,5 +2,8 @@ Locales['en'] = {
   ['Paperin'] = 'Press [~g~E~w~] to add Bank Paper to Stock',
   ['inkin'] = 'Press [~g~E~w~] to add Ink to Stock',
   ['DiscordWebhookName'] = 'Counterfeit Cash Script',
-  ['CutFakeCash'] = 'Cut Counterfeit Notes'
+  ['CutFakeCash'] = 'Cut Counterfeit Notes',
+  ['StackingCash'] = 'Press [~g~E~w~] to Stacking Notes',
+  ['WashingNotes'] = 'Press [~g~E~w~] to Washing Notes'
+
 }
